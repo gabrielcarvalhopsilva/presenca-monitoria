d9cecde06b270fc781a7825f1f3f231fc37b4d61
monitoria <presencadamonitoria@gmail.com>
2023-08-06 12:36:27 -0300

    starting design

--- presenca-monitoria/src/App.js
+++ presenca-monitoria/src/App.js
@@ -4,20 +4,47 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" */}
-        <p>
-          aaaaaaa vo reprova em fmc
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="container" action="">
+        <h1>Presença da monitoria</h1>
+        <div className="divContainer">
+          <div className="inputContainer">
+            <label>Nome</label>
+            <input placeholder="Ex: Gabriel Carvalho"></input>
+          </div>
+          <div className="inputContainer">
+            <label>Matricula</label>
+            <input placeholder="Ex: 20191011120005"></input>
+          </div>
+          <div className="inputContainer">
+            <label>Turma</label>
+            <input placeholder="Ex: T02"></input>
+          </div>
+          <div className="inputContainer">
+            <button>Enviar</button>
+          </div>
+        </div>
+      
+
+
+
+
+      {/*<
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"
+          <p>
+            aaaaaaa vo reprova em fmc
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        >*/}
+      </form>
     </div>
   );
 }
